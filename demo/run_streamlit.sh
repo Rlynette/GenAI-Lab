66cdfb4966bb9a59c5c40348e6b80e1ecd981f84
Lynette Ronnoh <lynetteronnoh86@gmail.com>
2025-11-09 18:34:55 +0300

    Restore demo from feat/ux-demo into feat/codebase-genius

---
+++ demo/run_streamlit.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Run streamlit demo (use in Terminal B)
+# Assumes virtualenv activated and dependencies installed (see demo/requirements.txt)
+streamlit run demo/streamlit_app.py --server.port 8501
